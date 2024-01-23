d98033e6f6bbc0a5ef2d64ddec137b54a206e059
gatici <gulsum.atici@canonical.com>
2024-01-23 18:07:01 +0300

    Initial Commit
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "model_name" {
+  description = "Name of Juju model to deploy application to."
+  type        = string
+  default     = ""
+}
+
+variable "channel" {
+  description = "The channel to use when deploying a charm."
+  type        = string
+  default     = "1.3/edge"
+}
+
+variable "amf_application_name" {
+  description = "The name of the AMF application."
+  type        = string
+  default     = "amf"
+}
+
+
